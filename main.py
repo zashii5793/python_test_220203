574dae4de20cc5df93aa2939c315a3cd139ebaa9
zashii5793 <hideki.ishizashi@gmail.com>
2022-02-05 15:31:17 +0900

    228th

--- main.py
+++ main.py
@@ -19,3 +19,23 @@ print('a')
 #今回の場合、古いバージョンの
 #pipコマンドがハッシュテーブルに記憶されているので、
 #下記コマンド「hash -r」で情報を消去しましょう。
+
+#pyenv: /Users/zashii/.pyenv/versions/3.10.2 already exists
+
+#あなたのホームディレクトリはどこなのか？…は環境変数 $HOME の中身を見ればわかります。
+
+# % echo $HOME
+
+超重要
+#切り替え方法
+https://teratail.com/questions/57641
+
+#Vimファイル編集方法
+https://programming-jissen.com/how-to-edit-files-with-vim/
+#
+
+#Vim ファイル編集の方法
+https://qiita.com/jintz/items/d357478271179c90ffab
+
+#ログインシェルとなった場合は↓設定による
+https://qiita.com/ryutoyasugi/items/cb895814d4149ca44f12
