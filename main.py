7ee2862c6009101416780d89a51f873fe7ddc3e4
zashii5793 <hideki.ishizashi@gmail.com>
2022-02-11 08:57:21 +0900

    234th

--- main.py
+++ main.py
@@ -15,3 +15,25 @@ for s in li[:]:
         list_a.append(s)
 
 list_a
+
+for i in range(0,16,4):
+    print(i)
+
+for i in range(0,16,4):
+    print(i,end=",")    
+
+a=[4,18,1,10,15,19]
+
+for i in range(6):
+    b=5+a[i]
+    if b>=20:
+        print("  skip")
+        continue
+    print(b,i,a[i])    
+
+def janken(name, content='tyoki',kaisu=3):
+    print(name,end=',')
+    print(content,end=',')
+    print(kaisu)
+
+janken(name='太郎',content='par')
