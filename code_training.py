2925e48cd344957e04a17fecb84993c542851df5
zashii5793 <hideki.ishizashi@gmail.com>
2022-02-16 07:39:41 +0900

    237th

--- code_training.py
+++ code_training.py
@@ -1,5 +1,10 @@
-a = 0
 
-text = f'これは{a}円です'
+data =[]
+for j in range(2,10):
+    data.append(j**2)
+print(data)
 
-print(text)
+names=["kuroki","aoyoma","settei"]
+for n in sorted(names):
+    print(n,end=",")
+    
