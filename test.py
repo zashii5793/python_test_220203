dda7a68adf726cf94955e9e4f09aff055d6bfd0e
zashii5793 <hideki.ishizashi@gmail.com>
2022-03-11 11:49:05 +0900

    243th

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 # resuests モジュールをインポート
 import requests
+from bs4 import BeautifulSoup
 
 # Webサイトの情報を変数rに格納し、そのうちのテキスト部分を出力
 r = requests.get("https://news.yahoo.co.jp/")
-print(r.text)
+soup = BeautifulSoup(r, "html.parser")
+print(r)
